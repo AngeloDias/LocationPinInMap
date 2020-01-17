ec7d78b95f610c827a47f6ff9a4634052430afaa
angelos <angelos@motorola.com>
2020-01-17 17:37:48 -0300

    Starting fragment test setting

--- app/src/androidTest/java/self/training/locationpininmap/MapsActivityInstrumentedTest.kt
+++ app/src/androidTest/java/self/training/locationpininmap/MapsActivityInstrumentedTest.kt
@@ -1,19 +1,35 @@
 package self.training.locationpininmap
 
+import androidx.fragment.app.FragmentManager
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import org.hamcrest.Matchers
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import self.training.locationpininmap.MapsActivity.Companion.fragmentPinsDialogTag
 
+
 @RunWith(AndroidJUnit4::class)
 class MapsActivityInstrumentedTest {
+    private lateinit var supportFrag: FragmentManager
+
     @get: Rule
     val intentsTestRule = IntentsTestRule(MapsActivity::class.java)
+
+    @get: Rule
+    val activityTestRule = ActivityTestRule<MapsActivity>(MapsActivity::class.java)
+
+    @Before
+    fun setUPFragment() {
+        supportFrag = activityTestRule.activity.supportFragmentManager
+
+        supportFrag.beginTransaction()
+    }
 
     @Test
     fun testing(){
